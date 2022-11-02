b33a89aa5c8cf617fddf7ab8a86cff5bed53f754
Kate Grant <hello.kategrant@gmail.com>
2022-11-02 15:31:21 -0400

    fixed server name

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432:stars",
+  process.env.DATABASE_URL || "postgres://localhost:5432/stars",
   {
     logging: false,
     // other options
