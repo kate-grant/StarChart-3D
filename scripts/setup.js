7961089f95d791606d7b9dda98ce72012e3293e3
Kate Grant <hello.kategrant@gmail.com>
2022-11-02 17:18:53 -0400

    use streams to unzip data

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,16 +1,25 @@
-import { exec } from "child_process";
+import zlib from "zlib";
 import fs from "fs";
 import seed from "../seed.js";
+import path from "path";
 
-if (!fs.existsSync("../data/hygdata_v3.csv")) {
-  exec("gunzip ../data/hygdata_v3.csv.gz", (err) => {
-    if (err) {
-      return;
-    }
-    console.log("Completed unzip");
-  });
-}
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const zipped = path.resolve(__dirname, "../data", "hygdata_v3.csv.gz");
+const unzipped = path.resolve(__dirname, "../data", "hygdata_v3.csv");
+console.log("zipped", zipped);
+console.log("1");
 
-seed();
+const unzip = zlib.createUnzip();
 
+const setup = () => {
+  if (!fs.existsSync(unzipped)) {
+    const input = fs.createReadStream(`${zipped}`);
+    const output = fs.createWriteStream(`${unzipped}`);
+    input.pipe(unzip).pipe(output);
+  }
+  console.log("6");
+
+  seed();
+};
+setup();
 console.log;
