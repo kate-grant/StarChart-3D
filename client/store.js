1a92776a7257b20e4933423b423c2133f8f0ff71
Kate Grant <hello.kategrant@gmail.com>
2022-11-03 18:45:28 -0400

    add axios to middleware args

--- client/store.js
+++ client/store.js
@@ -1,17 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
-import reducer from "./redux/reducer";
-import starReducer from "./redux/starReducer";
+import reducer from "./redux/reducer.js";
+import starReducer from "./redux/starReducer.js";
 //import auth from "./redux/auth";
 //import { combineReducers } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import { composeWithDevTools } from "@redux-devtools/extension";
+import axios from "axios";
 
-//let middleware = [thunkMiddleware.withExtraArgument({ axios })];
+let middleware = [thunkMiddleware.withExtraArgument({ axios })];
 
 const store = createStore(
   starReducer,
-  composeWithDevTools(applyMiddleware(thunkMiddleware, createLogger()))
+  composeWithDevTools(applyMiddleware(...middleware, createLogger()))
 );
 
 export default store;
