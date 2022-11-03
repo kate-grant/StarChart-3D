3c4efa2026cf52fc7bb147a07c4758ccef186b67
Kate Grant <hello.kategrant@gmail.com>
2022-11-03 18:49:26 -0400

    add client/components/StarChart.js

--- client/components/StarChart.js
+++ client/components/StarChart.js
@@ -1,19 +1,106 @@
-import React from "react";
+import React, { StrictMode, useEffect } from "react";
+import { connect } from "react-redux";
 import * as THREE from "three";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useThree, useFrame, calculateDpr } from "@react-three/fiber";
+import { fetchStars } from "../redux/starReducer.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as d3 from "d3";
 
-const StarChart = () => {
-  const nightsky = new THREE.Color(0x071b2b);
-  return (
-    <Canvas dpr={window.devicePixelRatio}>
-      <color attach="background" args={[nightsky]} />
-      {/* <ambientLight intensity={0.2} color={`#ffffff`} /> */}
-      {/* <mesh>
-        <sphereGeometry args={[1, 32, 32]} />
-        <meshStandardMaterial />
-      </mesh> */}
-    </Canvas>
-  );
+const CameraController = () => {
+  const { camera, gl } = useThree();
+  useEffect(() => {
+    const controls = new OrbitControls(camera, gl.domElement);
+
+    controls.enablePan = false;
+    //controls.endableRotate = true;
+    controls.enableZoom = true;
+    controls.minDistance = 20;
+    controls.maxDistance = 200;
+    controls.rotateSpeed = -1;
+    //return controls;
+    return () => {
+      controls.dispose();
+    };
+  }, [camera, gl]);
+  return null;
 };
 
-export default StarChart;
+class StarChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.Points = this.Points.bind(this);
+  }
+
+  async componentDidMount() {
+    try {
+      console.log("in componentdidmount");
+      await this.props.loadStars();
+      console.log(this.props.stars);
+    } catch (error) {
+      console.error();
+      throw error;
+    }
+  }
+
+  render() {
+    const nightsky = new THREE.Color(0x071b2b);
+    console.log("props", this.props);
+    const stars = this.props.stars;
+    const radius = d3.scaleLinear([6, -1], [0, 2]);
+    return (
+      <Canvas
+        camera={{
+          fov: 75,
+          aspect: window.innerWidth / window.innerHeight,
+          near: 1,
+          far: 1000,
+          position: [0, 0, 100],
+        }}
+      >
+        <CameraController />
+
+        <color attach="background" args={[nightsky]} />
+        {stars.map((star) => {
+          const r = Math.round(radius(star.magnitude) * 10) / 10;
+          if (r > 0) {
+            const pos = new THREE.Vector3().setFromSphericalCoords(
+              100,
+              Math.PI / 2 - star.dec,
+              star.ra
+            );
+
+            console.log("in map");
+            return (
+              <mesh position={pos}>
+                <sphereGeometry />
+                <meshStandardMaterial color="hotpink" />
+              </mesh>
+            );
+          }
+        })}
+        <pointLight position={[10, 10, 10]} />
+        <mesh position={[5, 10, 10]}>
+          <sphereGeometry />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+        <mesh position={[20, 50, 50]}>
+          <sphereGeometry />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+      </Canvas>
+    );
+  }
+}
+const mapState = (state) => {
+  return {
+    stars: state.stars,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    loadStars: () => dispatch(fetchStars()),
+  };
+};
+
+export default connect(mapState, mapDispatch)(StarChart);
