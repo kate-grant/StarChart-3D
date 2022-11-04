6efce391947751ce64b494f37dbc1a72ecffc411
Kate Grant <hello.kategrant@gmail.com>
2022-11-04 03:22:16 -0400

    added info text to info page

--- client/components/Info.js
+++ client/components/Info.js
@@ -8,12 +8,21 @@ const Info = () => {
         StarChart is a 3D star map built in JavaScript with React and React
         Three Fiber. There are many star map apps designed as interactive pocket
         guides for naked eye astronomy--these often come with a steep learning
-        curve to their granular options. In general, the gyroscoping nature that
-        makes these apps great at identifying local astral bodies can be a
-        disadvantage to users trying to learn the relative positions of
-        constellations to each other. StarChart provides a steady, self-guided
-        study aid for learning the relative positions of 88 officially
-        rocognized constellations.{" "}
+        curve to their granular options. Similarly, the gyroscoping features
+        that makes these apps great utilities for identifying local astral
+        bodies can be a disadvantageous to users aiming to learn the relative
+        positions of constellations to each other because of the limited view
+        and buouy-like movement. StarChart provides a steady, self-guided study
+        aid for learning the relative positions of 88 officially recognized
+        constellations.{" "}
+      </p>
+      <p>
+        Current star map defaults to the 1000 nearest starts to Earth. In the
+        future layers may be added to accomodate the full{" "}
+        <a href="https://github.com/astronexus/HYG-Database" target="_blank">
+          HYG Database dataset
+        </a>{" "}
+        used in this project.
       </p>
     </div>
   );
