ec345061c0e2ca5ec945f72e6f1e4367e3b0b8a1
Kate Grant <hello.kategrant@gmail.com>
2022-11-04 02:49:57 -0400

    added info page

---
+++ client/components/Info.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div id="info">
+      <h1>Info</h1>
+      <p>
+        StarChart is a 3D star map built in JavaScript with React and React
+        Three Fiber. There are many star map apps designed as interactive pocket
+        guides for naked eye astronomy--these often come with a steep learning
+        curve to their granular options. In general, the gyroscoping nature that
+        makes these apps great at identifying local astral bodies can be a
+        disadvantage to users trying to learn the relative positions of
+        constellations to each other. StarChart provides a steady, self-guided
+        study aid for learning the relative positions of 88 officially
+        rocognized constellations.{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Info;
