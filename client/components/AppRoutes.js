2ed5861850d3d0d49ef381a211fe9bb8f053d251
Kate Grant <hello.kategrant@gmail.com>
2022-11-04 02:49:07 -0400

    added sidebar

--- client/components/AppRoutes.js
+++ client/components/AppRoutes.js
@@ -1,15 +1,51 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home.js";
+import Info from "./Info.js";
 
 const AppRoutes = () => {
   return (
     <Router>
       <div>
-        <main>
+        <div id="sidebar">
+          <ul>
+            <li>
+              <Link to="/">
+                <span>
+                  <span class="material-symbols-outlined">auto_awesome</span>
+                </span>
+              </Link>
+            </li>
+            <li>
+              <a href="">
+                <span>
+                  <span class="material-symbols-outlined">insights</span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/kate-grant" target="_blank">
+                <span>
+                  <span class="material-symbols-outlined">code</span>
+                </span>
+              </a>
+            </li>
+            <li>
+              <Link to="/info">
+                <span>
+                  <span class="material-symbols-outlined">info</span>
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <nav>
           <h1>StarChart</h1>
+        </nav>
+        <main>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/info" element={<Info />} />
           </Routes>
         </main>
       </div>
--- client/components/Home.js
+++ client/components/Home.js
@@ -4,7 +4,6 @@ import StarChart from "./StarChart.js";
 const Home = () => {
   return (
     <div id="home">
-      <h2>This is the Home Page</h2>
       <StarChart />
     </div>
   );
