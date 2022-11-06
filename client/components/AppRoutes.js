f3d20b13c9738c2ed8150597c7a7f6f627998160
Kate Grant <hello.kategrant@gmail.com>
2022-11-06 00:31:10 -0400

    added settings component

--- client/components/AppRoutes.js
+++ client/components/AppRoutes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home.js";
 import Info from "./Info.js";
+import Settings from "./Settings.js";
 
 const AppRoutes = () => {
   return (
     <Router>
       <div>
+        <Settings />
         <div id="sidebar">
           <ul>
             <li>
